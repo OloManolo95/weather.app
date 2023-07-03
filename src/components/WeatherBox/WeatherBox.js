219e3179dedb0911d5e8d7bc2e82bac9f9b4f92e
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-07-03 12:33:26 +0200

    add weather state, pass weather data to WeatherSummary

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,12 +1,34 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback, useState } from 'react';
 
 const WeatherBox = props => {
+
+  const [weather, setWeather] = useState('');
+
+  const handleCityChange = useCallback( city  => {
+
+    const API_KEY = '49594c78db5894143b7ed090bb3643b8'
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ city }&appid=${API_KEY}&units=metric`)
+   .then(res => res.json())
+   .then(data => {
+     console.log(data);
+     const weatherData = {
+      city: data.name,
+      temp: data.main.temp,
+      icon: data.weather[0].icon,
+      description: data.weather[0].main
+      };
+      setWeather(weatherData);
+   });
+
+  }, []);
   return (
     <section>
-      <PickCity />
-      <WeatherSummary />
+      <PickCity action={handleCityChange}/>
+      <WeatherSummary {...weather} />
       <Loader />
     </section>
   )
